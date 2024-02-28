5a014d63b8910532dbd9edc749a9bec6b5273039
Ahmed Harabi <harabiahmed88@gmail.com>
2024-02-28 14:44:51 +0100

    project initilisation

---
+++ Graph.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
+public class Graph<T> {
+
+    private ArrayList<Vertex> Vertexes;
+    private ArrayList<ArrayList<Integer>> adjacencyMatrix;
+    
+
+
+
+
+    public int order(){
+        return Vertexes.size();
+    }
+    
+    public Vertex<T> vertexOf(T data){
+        for(Iterator<Vertex> i=Vertexes.iterator();i.hasNext();){
+
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
